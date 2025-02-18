4935014f548ad22e65d2ff4368db4fa9bd619cbc
Hermenegildo Kupessa <146777427+Sadboy2001Dev@users.noreply.github.com>
2025-02-18 12:00:29 +0100

    Primeira interacão com main

---
+++ Aula1/hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World\n");
+    return (0);
+};
+
+/*
+    int main() / int main(void) -> funcao principal
+    Onde roda todo o programa
+
+    printf -> comando de saida/ output
+
+    #include <stdio.h> : biblioteca de entrada e saida de dados (INPUT & OUTPUT)
+    todas as bibliotecas a serem incluidas devem ser antecipadas pela frase * #include <nome-da-biblioteca>*
+    o nome-da-biblioteca deve estar entre "<" & ">"
+
+    O programa feito é um programa que imprimime "Hello World" na tecla/ecrã
+*/
