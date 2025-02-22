7b37726fb477c18d41a343b4fd6b8f4062733f98
Hermenegildo Kupessa <146777427+Sadboy2001Dev@users.noreply.github.com>
2025-02-22 06:50:30 +0100

    Comandos de entrada de dados usados: getchar, fgets e scanf.

--- Aula5/entrada.c
+++ Aula5/entrada.c
@@ -10,15 +10,17 @@ int main()
     printf("Digite a sua idade: ");
     scanf("%d", &idade);
     
+    getchar(); //Para consumir o \n deixado pelo scanf no buffer
+
     printf("Informe O seu nome: ");
-    gets(nome);
+    fgets(nome, sizeof(nome), stdin); //em substituicao do gets
 
     puts("Comer te deixa feliz?");
     printf("Digite v - verdadeiro ou f - falso: ");
-    getchar(caracter);
+    caracter = getchar();
 
-    printf("Valor capturado usando o comando scanf: %d", idade);
-    printf("Valor capturado usando o comando gets: %s", nome);
+    printf("Valor capturado usando o comando scanf: %d\n", idade);
+    printf("Valor capturado usando o comando fgets: %s\n", nome);
     printf("Valor capturado usando o comando getchar: %c", caracter);
     
     return 0;
