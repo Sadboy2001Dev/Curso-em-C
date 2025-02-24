4f7c8c19a77ff75c158b3f81450aad3517f2e6eb
Hermenegildo Kupessa <146777427+Sadboy2001Dev@users.noreply.github.com>
2025-02-24 18:15:53 +0100

    Resto da divisao com o operadore Modulso (%)

---
+++ Aula8/resto.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // % (modulo)-> percentagem
+    // usado para calcuklar o resto da divisao
+    printf("Resto da divisao entre 3 e 2 = %i\n", 3 % 2);
+    printf("Resto da divisao entre 4 e 2 = %d\n", 4 % 2);
+    printf("Resto da divisao entre 5 e 2 = %d\n", 5 % 2);
+    printf("Resto da divisao entre 6 e 2 = %d\n", 6 % 2);
+    printf("Resto da divisao entre 11 e 3 = %d", 11 % 3);
+    return (0);
+}
