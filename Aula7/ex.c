1e74c2d14c1ff7333ed1c3a0359f35fb5ef97b72
Hermenegildo Kupessa <146777427+Sadboy2001Dev@users.noreply.github.com>
2025-02-24 18:07:43 +0100

    Sequenci de escapes

---
+++ Aula7/ex.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Esse é uma quebra de linha: \\n\n");
+    printf("\tEsse é a tabulacao: \\t");
+    printf("\n");
+    printf("Esse é uma aspas duplas: \" e \"");
+    printf("\n");
+    printf("Esse é uma aspas simples: \' e \'");
+    printf("\n");
+    printf("Esse é uma barra invertida: \\");
+    printf("\n");
+    printf("Esse indica o fim de um texto: \\0");
+    return (0);
+}
